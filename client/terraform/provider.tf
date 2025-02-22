dd2a5b7bc95ed11229c938a77865fd69339b64cf
Abhishek Tamboli <69687553+abdtamboli@users.noreply.github.com>
2025-02-22 11:21:26 -0500

    terraform configuration updated

--- client/terraform/provider.tf
+++ client/terraform/provider.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket"
+    bucket         = "househunt-terraform-state"
     key            = "househunt/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
